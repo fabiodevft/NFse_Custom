4bd81678279f575ecdca9e30de2fa69ff0617508
Fabio <fabiosousa.fulltime@gmail.com>
2019-10-16 10:43:05 -0300

    Ajuste de layout.

--- NFe.UI/Formularios/userConfiguracao_diversos.cs
+++ NFe.UI/Formularios/userConfiguracao_diversos.cs
@@ -481,7 +481,9 @@ namespace NFe.UI.Formularios
                            ufCod == 4313508 /*Osório-RS */ ||
                            ufCod == 4118006 /*Paraíso do Norte-PR*/ ||
                            ufCod == 4300604 /*Alvorada-RS*/ ||
-                           ufCod == 4104907 /*Castro-PR*/;
+                           ufCod == 4104907 /*Castro-PR*/ ||
+                           ufCod == 3516705 /*Garça-SP*/ ||
+                           ufCod == 3514502 /*Duartina-SP*/;
 
             lbl_UsuarioWS.Visible = txtUsuarioWS.Visible = lbl_SenhaWS.Visible = txtSenhaWS.Visible = visible;
         }
